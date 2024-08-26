ecb23007122d10d90d89b841e9d4efad549b7936
grqphical <95062977+grqphical@users.noreply.github.com>
2024-08-26 16:51:33 -0700

    added cmp

---
+++ .config/nvim/after/plugin/cmp.lua
@@ -0,0 +1,61 @@
+local cmp = require 'cmp'
+local lsp = require('lsp-zero')
+
+
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+
+        end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('lspconfig')['gopls'].setup {
+    capabilities = capabilities
+}
+require('lspconfig')['lua_ls'].setup {
+    capabilities = capabilities
+}
+require('lspconfig')['pyright'].setup {
+    capabilities = capabilities
+}
