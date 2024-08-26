231af1afdc21533566da5c8b9a739af218354dff
grqphical <95062977+grqphical@users.noreply.github.com>
2024-08-26 08:54:16 -0700

    added telescope select UI

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -30,6 +30,21 @@ return {
         tag = "0.1.8",
         dependencies = { 'nvim-lua/plenary.nvim' },
     },
+    {
+        'nvim-telescope/telescope-ui-select.nvim',
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+
+                        }
+                    }
+                }
+            }
+            require("telescope").load_extension("ui-select")
+        end
+    },
     { 'nvim-tree/nvim-web-devicons' },
     { 'neovim/nvim-lspconfig' },
     {
