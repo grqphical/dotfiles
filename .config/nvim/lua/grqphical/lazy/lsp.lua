99589307bc6b96ba0035d3e2f6432c0e43f4c021
grqphical <95062977+grqphical@users.noreply.github.com>
2024-08-26 17:47:48 -0700

    added custom remaps

--- .config/nvim/lua/grqphical/lazy/lsp.lua
+++ .config/nvim/lua/grqphical/lazy/lsp.lua
@@ -105,7 +105,7 @@ return {
                         header = "",
                         prefix = "",
                     },
-                })            
+                })         
             end
     },
 
--- .config/nvim/lua/grqphical/remap.lua
+++ .config/nvim/lua/grqphical/remap.lua
@@ -1,3 +1,23 @@
 vim.g.mapleader = " "
-
+-- Open NetRW
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Copy selection to clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Remap CTRL+C to esc
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- Apply code action
+vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action({ apply = true }) end)
+
+-- Format file
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format { async = true }
+end)
+
+-- Golang Specific: Insert if err != nil
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
+
+
