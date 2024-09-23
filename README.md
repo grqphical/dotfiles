d3d275245bd51e1acb61635c70bfd34910fa336b
grqphical <95062977+grqphical@users.noreply.github.com>
2024-09-22 17:28:29 -0700

    updated devenv script to use more FZF and attach to existing sessions, added license for scripts

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # grqphical's dotfiles
 
-These are my personal dotfiles for ZSH, Oh-My-Posh and Neovim running on Debian with WSL 2
+These are my personal dotfiles for ZSH, TMUX, Oh-My-Posh and Neovim running on Debian with WSL 2
+
+I also have included utility scripts of mine for things like creating reuseable TMUX sessions
 
 ## Install
 
@@ -18,3 +20,7 @@ git clone https://github.com/grqphical/dotfiles ~/dotfiles
 cd dotfiles
 stow .
 ```
+
+## License
+
+The dotfiles are public domain whilst any script in the `scripts/` folder is licensed under the MIT license, outlined in `LICENSE`
--- scripts/create-dev-env.sh
+++ scripts/create-dev-env.sh
@@ -1,32 +1,126 @@
-#!/bin/zsh
+#!/bin/bash
 # This script allows me to create development environments using TMUX in any folder I want
 # if the session already exists, TMUX attaches to it
+# 
+# Copyright 2024 grqphical
+# This script is licensed under the MIT license
+
+function template_go() {
+    tmux new-session -d -s $1 -n "editor"
+
+    tmux send-keys -t "$1:0" "cd \"$2\"" C-m
+    tmux send-keys -t "$1:0" "nvim ." C-m
+
+    tmux new-window -t "$1" -n "shell"
+    tmux send-keys -t "$1:1" "cd \"$2\"" C-m
+    tmux send-keys -t "$1:1" "go mod init \"$2\"" C-m
+    tmux send-keys -t "$1:1" "clear" C-m
+
+    tmux select-window -t "$1:0"
+}
+
+function template_rust() {
+    tmux new-session -d -s $1 -n "editor"
+
+    tmux send-keys -t "$1:0" "cd \"$2\"" C-m
+    tmux send-keys -t "$1:0" "cargo init --name $1" C-m
+    tmux send-keys -t "$1:0" "nvim ." C-m
+
+    tmux new-window -t "$1" -n "shell"
+    tmux send-keys -t "$1:1" "cd \"$2\"" C-m
+    tmux send-keys -t "$1:1" "clear" C-m
+
+    tmux select-window -t "$1:0"
+}
+
+function template_python() {
+    tmux new-session -d -s $1 -n "editor"
+
+    tmux send-keys -t "$1:0" "cd \"$2\"" C-m
+    tmux send-keys -t "$1:0" "nvim ." C-m
+
+    tmux new-window -t "$1" -n "shell"
+    tmux send-keys -t "$1:1" "cd \"$2\"" C-m
+    tmux send-keys -t "$1:1" "python3 -m venv venv" C-m
+    tmux send-keys -t "$1:1" ". venv/bin/activate" C-m
+    tmux send-keys -t "$1:1" "clear" C-m
+
+    tmux select-window -t "$1:0"
+}
+
+function template_js() {
+    tmux new-session -d -s $1 -n "editor"
+    
+    tmux send-keys -t "$1:0" "cd \"$2\"" C-m
+    tmux send-keys -t "$1:0" "npm create vite@latest ." C-m
+    tmux send-keys -t "$1:0" "nvim ." C-m
+
+    tmux new-window -t "$1" -n "shell"
+    tmux send-keys -t "$1:1" "cd \"$2\"" C-m
+    tmux send-keys -t "$1:1" "clear" C-m
+
+    tmux new-window -t "$1" -n "server"
+    tmux send-keys -t "$1:2" "cd \"$2\"" C-m
+    tmux send-keys -t "$1:2" "npm install" C-m
+    tmux send-keys -t "$1:2" "npm run dev" C-m
+
+    tmux select-window -t "$1:0"
+}
 
 cd ~
 
-# Prompt the user to select a directory using fzf
-SELECTED_DIR=$(find ~/development -type d | fzf --header "Choose folder to create environment in")
+if [[ "$1" == "new" ]]; then
+    # Prompt the user to select a directory using fzf
+    SELECTED_DIR=$(find ~/development -type d | fzf --header "Choose folder to create environment in")
 
-if [ -z "$SELECTED_DIR" ]; then
-    echo "No directory selected, exiting."
-    exit 1
+    if [ -z "$SELECTED_DIR" ]; then
+        echo "No directory selected, exiting."
+        exit 1
+    fi
+
+    SESH=$(basename "$SELECTED_DIR")
+
+    tmux has-session -t "$SESH" 2>/dev/null
+
+    if [ $? != 0 ]; then
+        TEMPLATE=$(echo -e "go\nrust\npython\njs" | fzf --header "Choose template")
+
+        if [[ -z "$TEMPLATE" ]]; then
+            echo "No template was chosen"
+            exit 1
+        fi
+
+        if [[ "$TEMPLATE" == "go" ]]; then
+            template_go $SESH $SELECTED_DIR
+        elif [[ "$TEMPLATE" == "rust" ]]; then
+            template_rust $SESH $SELECTED_DIR
+        elif [[ "$TEMPLATE" == "python" ]]; then
+            template_python $SESH $SELECTED_DIR
+        elif [[ "$TEMPLATE" == "js" ]]; then
+            template_js $SESH $SELECTED_DIR
+        fi
+    fi
+    tmux attach-session -t $SESH
+
+elif [[ "$1" == "attach" ]]; then
+    SESH=$(tmux list-sessions | cut -d':' -f1 | fzf --header "Choose a session")
+
+    if [ -z "$SESH" ]; then
+        echo "No session was chosen"
+        exit 1
+    fi
+    tmux attach-session -t "$SESH"
+else
+    echo "create-dev-env.sh by grqphical. A tool to create a TMUX session based on a template and/or folder"
+    echo ""
+    echo "Dependencies: tmux, fzf, cargo, npm, python3, go"
+    echo ""
+    echo "Commands: "
+    echo "  - help: Prints this message"
+    echo "  - new: Creates a new session"
+    echo "  - attach: Attaches to an existing session"
+    echo ""
+    echo "LICENSE: create-dev-env.sh is licensed under the MIT license"
 fi
 
-SESH=$(basename "$SELECTED_DIR")
 
-tmux has-session -t "$SESH" 2>/dev/null
-
-if [ $? != 0 ]; then
-    tmux new-session -d -s $SESH -n "editor"
-
-    tmux send-keys -t "$SESH:0" "cd \"$SELECTED_DIR\"" C-m
-    tmux send-keys -t "$SESH:0" "nvim ." C-m
-
-    tmux new-window -t "$SESH" -n "shell"
-    tmux send-keys -t "$SESH:1" "cd \"$SELECTED_DIR\"" C-m
-    tmux send-keys -t "$SESH:1" "clear" C-m
-
-    tmux select-window -t "$SESH:0"
-fi
-
-tmux attach-session -t $SESH
