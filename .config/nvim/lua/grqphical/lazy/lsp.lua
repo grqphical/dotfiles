bb117b9f6bdd5fb4f58850068c16da4da9cce17f
grqphical <95062977+grqphical@users.noreply.github.com>
2025-01-11 15:53:51 -0800

    restarted telescope config

--- .config/nvim/lua/grqphical/lazy/lsp.lua
+++ .config/nvim/lua/grqphical/lazy/lsp.lua
@@ -36,11 +36,29 @@ return {
 
             local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup {capabilities = capabilities}
-            lspconfig.pyright.setup{capabilities = capabilities}
-            lspconfig.gopls.setup{capabilities = capabilities}
-            lspconfig.emmet_language_server.setup{capabilities = capabilities}
+            lspconfig.lua_ls.setup { capabilities = capabilities }
+            lspconfig.pyright.setup { capabilities = capabilities }
+            lspconfig.gopls.setup { capabilities = capabilities }
+            lspconfig.emmet_language_server.setup { capabilities = capabilities }
 
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+                    if not client then
+                        return
+                    end
+
+                    if client.supports_method('textDocument/formatting') then
+                        vim.api.nvim_create_autocmd('BufWritePre', {
+                            buffer = args.buf,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                            end
+                        })
+                    end
+                end
+            })
         end
-    },
+    }
 }
--- .config/nvim/lua/grqphical/lazy/telescope.lua
+++ .config/nvim/lua/grqphical/lazy/telescope.lua
@@ -1,28 +1,20 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({})
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+        },
+        config = function()
+            local telescope = require('telescope.builtin')
 
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-			vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+            vim.keymap.set('n', '<leader>fd', telescope.find_files)
+            vim.keymap.set('n', '<leader>en', function()
+                telescope.find_files {
+                    cwd = vim.fn.stdpath('config')
+                }
+            end)
+        end,
+    },
 }
