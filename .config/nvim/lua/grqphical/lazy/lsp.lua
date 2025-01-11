f93bd8064d875cd8f954485f311bb27b213297e3
grqphical <95062977+grqphical@users.noreply.github.com>
2025-01-11 15:31:23 -0800

    added autocomplete

--- .config/nvim/lua/grqphical/lazy/lsp.lua
+++ .config/nvim/lua/grqphical/lazy/lsp.lua
@@ -11,11 +11,36 @@ return {
         },
     },
     {
+        'saghen/blink.cmp',
+        dependencies = 'rafamadriz/friendly-snippets',
+
+        version = '*',
+
+        opts = {
+            keymap = { preset = 'default' },
+
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                nerd_font_variant = 'mono'
+            },
+
+            signature = { enabled = true },
+
+        },
+        opts_extend = { "sources.default" }
+    },
+    {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+
             local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup {}
+            lspconfig.lua_ls.setup {capabilities = capabilities}
+            lspconfig.pyright.setup{capabilities = capabilities}
+            lspconfig.gopls.setup{capabilities = capabilities}
+            lspconfig.emmet_language_server.setup{capabilities = capabilities}
+
         end
     },
 }
