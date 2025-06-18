337739b6fbdb868fb09f877da8be82c22c880b0d
Nathan Jacobson <njacobson604@gmail.com>
2025-06-18 16:36:47 -0700

    chore: 🧹 updated lsp.lua

--- .config/nvim/lua/grqphical/lazy/lsp.lua
+++ .config/nvim/lua/grqphical/lazy/lsp.lua
@@ -43,10 +43,25 @@ return {
             lspconfig.lua_ls.setup { capabilities = capabilities }
             lspconfig.pyright.setup { capabilities = capabilities }
             lspconfig.gopls.setup { capabilities = capabilities }
-            lspconfig.emmet_language_server.setup { capabilities = capabilities }
+            lspconfig.templ.setup { capabilities = capabilities }
             lspconfig.html.setup { capabilities = capabilities }
             lspconfig.cssls.setup { capabilities = capabilities }
             lspconfig.jsonls.setup { capabilities = capabilities }
+            lspconfig.ts_ls.setup { capabilities = capabilities, init_options = {
+                plugins = {
+                    {
+                        name = "@vue/typescript-plugin",
+                        location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+                        languages = { "javascript", "typescript", "vue" },
+                    },
+                },
+            },
+                filetypes = {
+                    "javascript",
+                    "typescript",
+                    "vue",
+                }, }
+            lspconfig.emmet_language_server.setup { capabilities = capabilities, filetypes = { "html", "templ" } }
 
             vim.api.nvim_create_autocmd('LspAttach', {
                 callback = function(args)
