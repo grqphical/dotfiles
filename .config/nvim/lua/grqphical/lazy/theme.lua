aaad8133dff2db5fe64cfd6d27ff4e1d2c801aee
Nathan Jacobson <njacobson604@proton.me>
2025-07-27 14:45:34 -0700

    added background to signature help popup

--- .config/nvim/lua/grqphical/lazy/theme.lua
+++ .config/nvim/lua/grqphical/lazy/theme.lua
@@ -5,7 +5,8 @@ return {
         vim.cmd("colorscheme rose-pine")
 
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "TelescopeNormal", {})
+        vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
         vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
         vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
